8eb7e38dab3c524347d712543c575793dc89a03a
GodishalaAshwith <ashwith7777@gmail.com>
2024-11-02 20:47:25 +0530

    Responsive-1
    Responsive and better

--- Portfolio-2024/script.js
+++ Portfolio-2024/script.js
@@ -1,27 +1,62 @@
 const hamburger = document.querySelector(".hamburger");
+const hamMenu = document.querySelector(".ham-tags");
+const imageSlide = document.querySelector(".certifications");
+const gallery = document.querySelector(".gallery");
 
-const imageSlide = document.querySelector(".certifications")
+// Hamburger Menu
 
 const toggleMenu = () => {
-  const hamMenu = document.querySelector(".ham-tags");
   hamMenu.classList.toggle("active");
-}
+};
 
-hamburger.addEventListener('click' , toggleMenu );
+const clicksOutside = (event) => {
+  if (!hamMenu.contains(event.target) && !hamburger.contains(event.target)) {
+    hamMenu.classList.remove("active");
+  }
+};
 
-const urls = [
+hamburger.addEventListener("click", toggleMenu);
+document.addEventListener("click", clicksOutside);
+
+// Certificates
+
+const urlsCertis = [
   "assests/certifications/Drones_Workshop_page-0001.jpg",
   "assests/certifications/EnglishWriting_page-0001.jpg",
   "assests/certifications/GitCryptex_page-0001.jpg",
   "assests/certifications/PythonBasics_page-0001.jpg",
-  "assests/certifications/Srujana_Volunteering_page-0001.jpg"
+  "assests/certifications/Srujana_Volunteering_page-0001.jpg",
 ];
 
 let currentIndex = 0;
 
 const toggleImage = () => {
-  currentIndex = (currentIndex + 1) % urls.length;
-  imageSlide.style.backgroundImage = `url(${urls[currentIndex]})`;
+  currentIndex = (currentIndex + 1) % urlsCertis.length;
+  imageSlide.style.backgroundImage = `url(${urlsCertis[currentIndex]})`;
 };
 
-imageSlide.addEventListener('click', toggleImage)
+imageSlide.addEventListener("click", toggleImage);
+
+// Projects
+
+const urlsProjects = [
+  "https://amazon-proj-by-ash.netlify.app/",
+  "https://github.com/GodishalaAshwith/youtube-clone-proj",
+  "https://rock-paper-scissors-byash.netlify.app/",
+  "https://tic-tac-toe-byash.netlify.app/",
+  "https://weather-app-by-ash.netlify.app/",
+];
+
+const imgBoxes = gallery.children;
+console.log(imgBoxes);
+
+for (let i = 0; i < imgBoxes.length; i++) {
+  imgBoxes[i].addEventListener(
+    "click",
+    (() => {
+      return () => {
+        window.open(urlsProjects[i], "_blank");
+      };
+    })()
+  );
+}
